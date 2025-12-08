9ddc9107963760b5c1cd12977e16b29beb36dc26
btpp03 <btpp03@gmail.com>
2025-12-08 18:41:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const { WebSocket, createWebSocketStream } = require('ws');
 const UUID = process.env.UUID || 'faacf142-dee8-48c2-8558-641123eb939c';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezhak2.btpp.ggff.net';
 const NEZHA_PORT = process.env.NEZHA_PORT || '443';
-const NEZHA_KEY = process.env.NEZHA_KEY || 'gcOpVFikjcmdZ0my4m';
+const NEZHA_KEY = process.env.NEZHA_KEY || 'InfKxivl5ubv3J0dZA';
 const DOMAIN = process.env.DOMAIN || 'empire.kingsnetwork.uk';
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;
 const WSPATH = process.env.WSPATH || UUID.slice(0, 8);
 const SUB_PATH = process.env.SUB_PATH || 'sub';
 const NAME = process.env.NAME || '';
-const PORT = process.env.PORT || 25949;
+const PORT = process.env.PORT || 25927;
 
 let uuid = UUID.replace(/-/g, ""), CurrentDomain = DOMAIN, Tls = 'tls', CurrentPort = 443, ISP = '';
 const vmsUser = { id: UUID, alterId: 0, security: 'auto' };
